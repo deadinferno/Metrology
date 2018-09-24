7883747f00c7d5b82aeb3f6e979a6b51fd6c0a69
Vladimir <38225120+deadinferno@users.noreply.github.com>
2018-09-24 12:49:58 +0300

    upd

---
+++ lab01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace lab01
+{
+    class Program
+    {
+        static void Main()
+        {
+            string someStr;
+            Console.Write("Исхд. строка: ");
+            someStr = Console.ReadLine();
+            someStr = compress(someStr);
+            Console.WriteLine("Форм. строка: " + someStr);
+        }
+        static string compress(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                str = str.Replace(" ", string.Empty);
+            }
+            return str;
+        }
+    }
+}
