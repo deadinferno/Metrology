303910608bdd98b3ec212f86424028c732fc4088
Vladimir <38225120+deadinferno@users.noreply.github.com>
2018-10-01 22:53:42 +0300

    ываы

--- lab01/Program.cs
+++ lab01/Program.cs
@@ -8,16 +8,8 @@ namespace lab01
             string someStr;
             Console.Write("Исхд. строка: ");
             someStr = Console.ReadLine();
-            someStr = compress(someStr);
+            someStr = someStr.Replace(" ", string.Empty);
             Console.WriteLine("Форм. строка: " + someStr);
-        }
-        static string compress(string str)
-        {
-            for (int i = 0; i < str.Length; i++)
-            {
-                str = str.Replace(" ", string.Empty);
-            }
-            return str;
         }
     }
 }
